bb8dbd6ed2605c9fe87a7736d3921f06a367bc84
Artem2335 <art20072975@gmail.com>
2025-12-17 20:20:26 +0300

    Fix SQLAlchemy 2.0 syntax for text() in server_default

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import text, String
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base, str_uniq, int_pk
 from typing import List, TYPE_CHECKING
@@ -17,9 +17,9 @@ class User(Base):
     password: Mapped[str]
     username: Mapped[str]
 
-    is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
-    is_moderator: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
-    is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
+    is_user: Mapped[bool] = mapped_column(default=True, server_default="1", nullable=False)
+    is_moderator: Mapped[bool] = mapped_column(default=False, server_default="0", nullable=False)
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default="0", nullable=False)
 
     # Relationships
     reviews: Mapped[List["Review"]] = relationship("Review", back_populates="user", cascade="all, delete-orphan", foreign_keys="Review.user_id")
